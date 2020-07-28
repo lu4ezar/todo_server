915fed593a75eb67ed8a6c62801177f291104847
lu4ezar <misha-shisha@mail.ru>
2020-07-28 21:06:37 +0700

    change webpack output dir to 'public'

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   resolve: {
     extensions: ['.ts', '.js'],
