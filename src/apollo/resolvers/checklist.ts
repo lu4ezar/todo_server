26ad0470a39d082a88437d74b3df45092dcd8508
lu4ezar <misha7shisha@gmail.com>
2020-11-13 19:39:57 +0700

    temp

---
+++ src/apollo/resolvers/checklist.ts
@@ -0,0 +1,26 @@
+
+import {
+  Resolvers,
+  Checklist,
+  QueryResolvers,
+  MutationResolvers,
+} from '../generated/graphql';
+// import { IChecklist } from '../mongoose/checklist.interface';
+
+const resolvers: Resolvers = {
+  Query: {
+    checklist: (_, { id }, { dataSources }): Checklist =>
+      dataSources.checklistAPI.getChecklist(id),
+    checklists: (_, __, { dataSources }): Checklist[] => dataSources.checklistsAPI.getChecklists(),
+  } as QueryResolvers,
+  Mutation: {
+    createChecklist: async (_, { input }, { dataSources }): Promise<Checklist> =>
+      dataSources.checklistsAPI.createChecklist(input),
+    updateChecklist: async (_, { id, input }, { dataSources }): Promise<Checklist> =>
+      dataSources.checklistsAPI.updateChecklist(id, input),
+    deleteChecklist: async (_, { id }, { dataSources }): Promise<Checklist> =>
+      dataSources.checklistsAPI.deleteChecklist(id),
+  } as MutationResolvers,
+};
+
+export default resolvers;
