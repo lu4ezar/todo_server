235d49adeb2ac5533a81eb4215a1cd522dba3a84
lu4ezar <misha7shisha@gmail.com>
2020-11-17 14:17:22 +0700

    User typeDef

---
+++ src/apollo/typeDefs/user.ts
@@ -0,0 +1,39 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+  """
+  User Type
+	"""
+	
+  type User {
+		id: ID!
+		email: String!
+		hashedPassword: String!
+		roles: [String!]!
+		created: DateTime!
+	}
+	
+	extend type Query {
+    me: User!
+    users: [User!]!
+  }
+
+  extend type Mutation {
+	createUser(input: CreateUserInput!): User!
+    updateUser(input: UpdateUserInput!): User!
+    deleteUser(id: ID!): User!
+	}
+	
+	input CreateUserInput {
+    email: String!
+    hashedPassword: String!
+  }
+
+  input UpdateUserInput {
+    id: ID!
+    email: String
+    hashedPassword: String
+  }
+
+`;
+
