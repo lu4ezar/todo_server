ebe07f6e6071fed2eb92b7af8ef33958f6f4deb8
lu4ezar <misha7shisha@gmail.com>
2020-11-27 12:24:44 +0700

    update User

--- src/apollo/resolvers/users.ts
+++ src/apollo/resolvers/users.ts
@@ -2,17 +2,20 @@ import {
   Resolvers,
   QueryResolvers,
   MutationResolvers,
+  Token,
 } from '../../generated/graphql';
 import { IUser } from '../../mongoose/user.interface';
 
 const resolvers: Resolvers = {
   Query: {
-    me: (_, { email }, { dataSources }): Promise<IUser> =>
-      dataSources.usersAPI.getUser(email),
+    /* me: (_, { email }, { dataSources }): Promise<IUser> =>
+      dataSources.usersAPI.getUser(email),*/
   } as QueryResolvers,
   Mutation: {
-    createUser: async (_, { input }, { dataSources }): Promise<IUser> =>
+    createUser: async (_, { input }, { dataSources }): Promise<Token> =>
       dataSources.usersAPI.createUsers(input),
+    loginUser: async (_, { input }, { dataSources }): Promise<Token> =>
+      dataSources.usersAPI.loginUser(input),
     updateUser: async (_, { input }, { dataSources }): Promise<IUser> =>
       dataSources.usersAPI.updateUser(input),
     /* deleteUser: async (_, { email }, { dataSources }): Promise<IUser> =>
