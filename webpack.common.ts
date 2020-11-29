29522e56cfb8ca9c25f005e7f79aa7667e32311e
lu4ezar <misha-shisha@mail.ru>
2020-11-29 18:36:27 +0700

    cleanup

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,25 +1,5 @@
 import path from 'path';
 
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         exclude: [path.resolve(__dirname, 'NODE_MODULES')],
-//         test: /\.ts$/,
-//         use: 'ts-loader',
-//       },
-//     ],
-//   },
-//   output: {
-//     filename: 'index.js',
-//     path: path.resolve(__dirname, 'public'),
-//   },
-//   resolve: {
-//     extensions: ['.ts', '.js'],
-//   },
-//   target: 'node',
-// };
-
 export default {
   module: {
     rules: [
