185d166590d85d4c7e98175fd21130939f88f869
lu4ezar <misha-shisha@mail.ru>
2020-12-26 20:03:32 +0700

    add Id to User (not sure yet why)

--- src/mongoose/user.interface.ts
+++ src/mongoose/user.interface.ts
@@ -1,7 +1,9 @@
 import { Document } from 'mongoose';
+import { Scalars } from '../generated/graphql';
 import { IChecklistRefDocument } from './checklist.interface';
 
 export interface IUser extends Document {
+  id: Scalars['ID'];
   email: string;
   created: Date;
   checklists: IChecklistRefDocument['_id'][];
