6b3998117bf3a3a5580e4585140f3e3a22995047
lu4ezar <misha-shisha@mail.ru>
2020-12-26 11:40:35 +0700

    add User queries resolvers

--- src/apollo/resolvers/users.ts
+++ src/apollo/resolvers/users.ts
@@ -1,16 +1,18 @@
 import {
   Resolvers,
-  // QueryResolvers,
+  QueryResolvers,
   MutationResolvers,
   AuthPayload,
 } from '../../generated/graphql';
 import { IUser } from '../../mongoose/user.interface';
 
 const resolvers: Resolvers = {
-  // Query: {
-  //   user: (_, { email }, { dataSources, req }): Promise<IUser> =>
-  //     dataSources.usersAPI.getUser(email),
-  // } as QueryResolvers,
+  Query: {
+    user: (_, { email }, { dataSources }): Promise<IUser> =>
+      dataSources.usersAPI.getUser(email),
+    users: (_, __, { dataSources }): Promise<IUser[]> =>
+      dataSources.usersAPI.getUsers(),
+  } as QueryResolvers,
   Mutation: {
     createUser: async (_, { input }, { dataSources }): Promise<AuthPayload> =>
       dataSources.usersAPI.createUser(input),
