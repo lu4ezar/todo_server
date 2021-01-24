e2f2e2d3f90ed6ae4fae22bdf1d6b641b44d06cc
lu4ezar <misha-shisha@mail.ru>
2021-01-24 22:29:23 +0700

    remove middleware

--- src/middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-import expressJwt from 'express-jwt';
-
-export default expressJwt({
-  secret: process.env.SECRET || '',
-  algorithms: ['HS256'],
-  credentialsRequired: false,
-});
--- src/middleware/index.ts
+++
@@ -1,5 +0,0 @@
-import auth from './auth';
-import cors from './cors';
-import parser from './parser';
-
-export default [auth, cors, parser];
