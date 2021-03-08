20fa0dd02391efbea80d8f8d05aa94b12532f372
lu4ezar <misha-shisha@mail.ru>
2021-03-08 20:23:00 +0700

    remove redundant types

--- src/apollo/datasources/checklists.ts
+++ src/apollo/datasources/checklists.ts
@@ -1,9 +1,6 @@
 import { DataSource } from 'apollo-datasource';
 import { Collection } from 'mongoose';
-import {
-  IChecklistDocument,
-  IChecklistRefDocument,
-} from '../../mongoose/interfaces/checklist.interface';
+import { IChecklistDocument } from '../../mongoose/interfaces/checklist.interface';
 import Checklist from '../../mongoose/models/checklist.model';
 import {
   Checklist as ChecklistType,
@@ -21,29 +18,27 @@ export default class ChecklistsAPI extends DataSource {
 
   // Queries
   async getChecklists(userId: User['id']): Promise<Array<IChecklistDocument>> {
-    return await Checklist.find({ owner: userId }).populate('todos').exec();
+    return await Checklist.find({ owner: userId });
   }
 
-  async getChecklist(_id: ChecklistType['id']): Promise<IChecklistRefDocument> {
-    return (await Checklist.findOne({ _id })) as IChecklistRefDocument;
+  async getChecklist(_id: ChecklistType['id']): Promise<IChecklistDocument> {
+    return (await Checklist.findOne({ _id })) as IChecklistDocument;
   }
   // Mutations
-  createChecklist(input: CreateChecklistInput): Promise<IChecklistRefDocument> {
-    const checklist = new Checklist(input) as IChecklistRefDocument;
+  createChecklist(input: CreateChecklistInput): Promise<IChecklistDocument> {
+    const checklist = new Checklist(input) as IChecklistDocument;
     return checklist.save();
   }
   async updateChecklist(
     input: UpdateChecklistInput
-  ): Promise<IChecklistRefDocument> {
+  ): Promise<IChecklistDocument> {
     return (await Checklist.findOneAndUpdate({ _id: input.id }, input, {
       new: true,
-    })) as IChecklistRefDocument;
+    })) as IChecklistDocument;
   }
-  async deleteChecklist(
-    _id: ChecklistType['id']
-  ): Promise<IChecklistRefDocument> {
+  async deleteChecklist(_id: ChecklistType['id']): Promise<IChecklistDocument> {
     return (await Checklist.findOneAndDelete({
       _id,
-    })) as IChecklistRefDocument;
+    })) as IChecklistDocument;
   }
 }
